c589670271a29fec587d6a5cf6827148fe76db4e
hello-hyeju1125 <hyeju.park@studioynp.com>
2026-08-27 08:16:35 +0900

    책 추가: 인 더 메가처치

--- books.js
+++ books.js
@@ -740,4 +740,7 @@ const BOOKS = [
   { title: "마흔 즈음에", author: "서쿤스", publisher: "네이버웹툰", type: "webtoon", platform: "네이버웹툰", link: "https://comic.naver.com/webtoon/list?titleId=836052", status: "연재중", genres: ["드라마", "2024 연재직행열차", "고자극드라마"], published: null, readYear: 2026,
     cover: "https://image-comic.pstatic.net/webtoon/836052/thumbnail/thumbnail_IMAG21_b475b1ef-5d2a-4afe-a1b4-5b3c6cc577f7.jpg", isbn: "",
     summary: "어느 덧 마흔살이 된 현성민은 주변을 돌아보니 혼자가 되었다는 현실을 절실히 깨닫게 된다. 자신의 인생 뿐 아니라 직장에서도 위기를 맞게 된 현성민은 진정한 행복을 찾기 위해 고군분투 하기로 결심한다. 불혹의 나이, 아직 늦지 않았다!", review: "하이퍼리얼리즘. 빨리 결혼을 하든 더 화려한 싱글로 남든.", rating: 4, quotes: [] },
+  { title: "인 더 메가처치", author: "아사이 료", publisher: "은행나무", genres: [], published: 2026, readYear: 2026,
+    cover: "https://contents.kyobobook.co.kr/sih/fit-in/458x0/pdt/9791167376749.jpg", isbn: "9791167376749",
+    summary: "", review: "", rating: null, quotes: [] },
 ];
